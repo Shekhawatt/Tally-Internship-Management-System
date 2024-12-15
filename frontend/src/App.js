81d6549053b4034c08bea75155894379a6b632d6
Your Name <your_email@example.com>
2024-12-15 19:42:11 +0530

    Updated milestone section to turn green when all subtasks are completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
         <Route path="/apply" element={<ApplyPage />} />
 
         {/* Default route */}
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" element={<ApplyPage />} />
       </Routes>
     </Router>
   );
